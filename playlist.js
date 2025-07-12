b752f0e143ee1812d959eeaadb4bf25aa1414880
ozyurtyakup01 <ozyurt_yakup01@student.smc.edu>
2025-07-12 13:17:35 -0700

    some comments added

---
+++ playlist.js
@@ -0,0 +1,45 @@
+// default constructor
+function Playlist(name) {
+  this.name = name;
+  this.songs = [];
+  this.currentSong = null;
+}
+// push string to songs array
+Playlist.prototype.addSong = function(songTitle) {
+  this.songs.push(songTitle);
+};
+
+// sets currentSong to first element of the array
+Playlist.prototype.playFirst = function() {
+  //check if array is empty
+  if (this.songs.length > 0) {
+    //if not empty set current song to first element
+    this.currentSong = this.songs[0];
+    // print new current song
+    console.log("Now playing:", this.currentSong);
+  }
+};
+
+Playlist.prototype.skipSong = function() {
+  if (this.songs.length > 1) {
+    this.songs.shift();
+    this.currentSong = this.songs[0];
+    console.log("Skipped! Now playing:", this.currentSong);
+  } else {
+    console.log("No more songs to skip.");
+  }
+};
+
+Playlist.prototype.listSongs = function() {
+  console.log("Playlist:", this.name);
+  console.log("Songs:", this.songs.join(", "));
+};
+
+
+let myMix = new Playlist("My Chill Mix");
+myMix.addSong("Lofi Study");
+myMix.addSong("Chillhop Beats");
+myMix.addSong("Evening Jazz");
+myMix.playFirst();
+myMix.skipSong();
+myMix.listSongs();
